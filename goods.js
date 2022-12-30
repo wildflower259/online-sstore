fc14f14adca644ab4e8a623e3ce93d9ee146063c
wildflower259 <85477814+wildflower259@users.noreply.github.com>
2022-12-30 10:50:31 +0900

    Add files via upload

---
+++ goods.js
@@ -0,0 +1,14 @@
+const GOODS = [{
+	id: 1,
+	imgSrc: 'мольберт.png',
+}, {
+	id: 2,
+	name: 'Набор кистей',
+	price: 1509,
+	imgSrc: 'кисти.png',
+}, {
+	id: 3,
+	name: 'Краски гуашевые',
+	price: 260,
+	imgSrc: 'краски.png',
+}];
